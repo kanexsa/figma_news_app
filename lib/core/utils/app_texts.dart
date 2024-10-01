04c83451f7bfae882c4e7f639ee4c6c6a6c67cce
udana <udana@denizli.bel.tr>
2024-10-01 16:06:20 +0300

    login screen completed

--- lib/core/utils/app_texts.dart
+++ lib/core/utils/app_texts.dart
@@ -23,4 +23,30 @@ class AppTexts {
   static const String login = "Login";
   static const String forgotPassword = "Forgot Password";
   static const String dontHaveAnAccount = "Don’t have an account? Join us";
+  static const String notVerifyEmailCheck =
+      "Your email address is not verified. Please check your email.";
+  static const String notVerifyEmail = "Your email address is not verified.";
+  static const String userNotFoundCode = "user-not-found";
+  static const String userNotFoundError =
+      "No registered user was found with this email address";
+  static const String wrongPasswordCode = "wrong-password";
+  static const String wrongPasswordError = "You entered the wrong password";
+  static const String invalidEmailCode = "invalid-email";
+  static const String invalidEmailError = "Invalid email address.";
+  static const String invalidCredentialCode = "invalid-credential";
+  static const String invalidCredentialError =
+      "An error occurred while logging in with your email address. Please check your email.";
+  static const String defaultError = "An error has occurred. Please try again.";
+  static const String defaultLoginError = "Login failed. Please try again.";
+  static const String defaultSignUpError =
+      "Registration failed. Please try again.";
+  static const String nameError = "Name cannot be empty.";
+  static const String validEmailError = "Invalid email format.";
+  static const String emailError = "Email cannot be empty.";
+  static const String passwordError = "Password cannot be empty.";
+  static const String privacyPolicyError =
+      "You must agree to the privacy policy.";
+  static const String bottomSheetForgotPasswordDesc =
+      "Enter your email for the verification proccess, we will send 4 digits code to your email.";
+  static const String continueText = "Continue";
 }
